329505102e0e9b5cf561c6689d94c79c2c1a105b
Gusev Vova <volkagu@yandex.ru>
2024-04-12 13:58:40 +0300

    start project

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import asyncio
+import logging
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.bot import DefaultBotProperties
+from bot.handlers import router
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY_TG')
+debug=True
+
+async def main():
+    bot = Bot(API_KEY, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    try:
+        logging.basicConfig(level=logging.INFO)
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.error('KeyboardInterrupt')
+        
+
