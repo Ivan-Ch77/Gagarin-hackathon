82370c3ac232d39128cd680a450e81e2fb57bbd4
Иван Чернядьев <chim20r077@student.bmstu.ru>
2024-04-12 12:42:52 +0300

    Add sending request to YandexGPT

---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+
+catalogID = "b1gnm8ek6qvl7tnkh3tn"
+secret_key = "AQVN02Y3W4I5HDxd7lFmmSLXhU4vv9Vh5baeRqpn"
+
+prompt = {
+    "modelUri": f"gpt://{catalogID}/yandexgpt-lite",
+    "completionOptions": {
+        "stream": True,
+        "temperature": 0.8,
+        "maxTokens": "2000",
+    },
+
+    # даем пример контекста беседы
+    "messages": [
+        # system - имитируем систему
+        # user - имитируем пользователя
+        # assistant - имитируем самого бота, который будет отвечать
+        {
+            "role": "system",
+            "text": "Ты оригинально заполняешь эпиграфию."
+        },
+        {
+            "role": "user",
+            "text": "Я заполняю поля в опросе об одном человеке, помоги мне креативно заполнить их."
+        },
+        {
+            "role": "assistant",
+            "text": "Привет! Напишите пожалуйста базовую информацию об этом человеке"
+        },
+        {
+            "role": "user",
+            "text": "Хорошо, помоги мне на основании этих данных написать эпиграфию."
+        },
+    ]
+}
+
+url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Api-Key {secret_key}"
+}
+
+response = requests.post(url, headers=headers, json=prompt)
+result = response.text
+print(result)
