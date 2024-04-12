e34e21d5adbf960df5cd3ab5ead8fae19012e740
Иван Чернядьев <chim20r077@student.bmstu.ru>
2024-04-12 13:52:25 +0300

    Edit sending request to YandexGPT Now we use aiohttp

--- main.py
+++ main.py
@@ -1,46 +1,60 @@
-import requests
+import aiohttp
+import asyncio
 
-catalogID = "b1gnm8ek6qvl7tnkh3tn"
-secret_key = "AQVN02Y3W4I5HDxd7lFmmSLXhU4vv9Vh5baeRqpn"
 
-prompt = {
-    "modelUri": f"gpt://{catalogID}/yandexgpt-lite",
-    "completionOptions": {
-        "stream": True,
-        "temperature": 0.8,
-        "maxTokens": "2000",
-    },
+async def post_request(url, headers, data):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers, json=data) as response:
+            return await response.text()
 
-    # даем пример контекста беседы
-    "messages": [
-        # system - имитируем систему
-        # user - имитируем пользователя
-        # assistant - имитируем самого бота, который будет отвечать
-        {
-            "role": "system",
-            "text": "Ты оригинально заполняешь эпиграфию."
+
+async def main():
+    catalogID = "b1gnm8ek6qvl7tnkh3tn"
+    secret_key = "AQVN02Y3W4I5HDxd7lFmmSLXhU4vv9Vh5baeRqpn"
+
+    prompt = {
+        "modelUri": f"gpt://{catalogID}/yandexgpt-lite",
+        "completionOptions": {
+            "stream": True,
+            "temperature": 0.8,
+            "maxTokens": "2000",
         },
-        {
-            "role": "user",
-            "text": "Я заполняю поля в опросе об одном человеке, помоги мне креативно заполнить их."
-        },
-        {
-            "role": "assistant",
-            "text": "Привет! Напишите пожалуйста базовую информацию об этом человеке"
-        },
-        {
-            "role": "user",
-            "text": "Хорошо, помоги мне на основании этих данных написать эпиграфию."
-        },
-    ]
-}
 
-url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Api-Key {secret_key}"
-}
+        # даем пример контекста беседы
+        "messages": [
+            # system - имитируем систему
+            # user - имитируем пользователя
+            # assistant - имитируем самого бота, который будет отвечать
+            {
+                "role": "system",
+                "text": "Ты оригинально заполняешь эпиграфию."
+            },
+            {
+                "role": "user",
+                "text": "Я заполняю поля в опросе об одном человеке, помоги мне креативно заполнить их."
+            },
+            {
+                "role": "assistant",
+                "text": "Привет! Напишите пожалуйста базовую информацию об этом человеке"
+            },
+            {
+                "role": "user",
+                "text": "Хорошо, помоги мне на основании этих данных написать эпиграфию."
+            },
+        ]
+    }
 
-response = requests.post(url, headers=headers, json=prompt)
-result = response.text
-print(result)
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Api-Key {secret_key}"
+    }
+
+    # response = requests.post(url, headers=headers, json=prompt)
+    # result = response.text
+
+    result = await post_request(url, headers, prompt)
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
